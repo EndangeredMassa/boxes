62acc1502af088d7c2cc2a8564e32f9432cca0c5
Sean Massa <smassa@groupon.com>
2014-02-18 15:20:47 -0600

    fix cli some more

--- cli.js
+++ cli.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 
-require(__dirname + '/lib');
+// for some reasons, `npm start` works,
+// but requireing the same module here does not
+require('child_process').spawn('npm', ['start'], {cwd: process.cwd, stdio: 'inherit'})
+
+// require(__dirname + '/lib/index.js');
 
